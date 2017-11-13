5d6de11f3f8268d0d0f8b20d3304c4be1dacb835
Ondřej Pauswang <o.pauswang@cz.gmc.net>
2017-11-13 16:46:13 +0100

    added icon to intro

--- src/pages/homePage/intro.ts
+++ src/pages/homePage/intro.ts
@@ -1,6 +1,9 @@
 import * as b from 'bobril';
 import * as styles from '../../components/styles';
 import * as AppButton from '../../components/appBar/button';
+import * as Colors from '../../components/colors';
+import * as Image from '../../components/image/lib';
+import * as assets from '../../assets/assets';
 
 interface IData {
 }
@@ -9,18 +12,32 @@ interface IContext extends b.IBobrilCtx {
     data: IData;
 }
 
+let headerStyle = styles.createBobrilStylefromObjects(styles.headertext01, { color: Colors.color04 });
+
 export const create = b.createComponent<IData>({
     render(ctx: IContext, me: b.IBobrilNode) {
         const d = ctx.data;
 
         me.children = [
-            b.styledDiv(['BOBRIL'], styles.headertext01), // styles.color04
-            //b.styledDiv(AppButton),
+            Image.create({
+                asset: assets.bobrilLogoWhiteIntro,
+                width: 300,
+                height: 284,
+                style: {
+                    margin: 'auto'
+                }
+            }),
+
+            b.styledDiv(['BOBRIL'], headerStyle),
+
         ];
 
         b.style(
             me,
-            
+            {
+                //height: '200px',
+                background: Colors.color02,
+            }
         );
     }
 });
