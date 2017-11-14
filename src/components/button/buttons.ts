a063cda7fece4a15422c6ece2d1cca39ac056c61
Ondřej Pauswang <o.pauswang@cz.gmc.net>
2017-11-14 14:27:09 +0100

    styles implementing via stylesDef interface

--- src/components/button/buttons.ts
+++ src/components/button/buttons.ts
@@ -1,21 +1,33 @@
 import * as b from 'bobril';
 
-
 export interface IButtonStyles {
     style: b.IBobrilStyleDef;
     hoverStyle?: b.IBobrilStyleDef;
 }
 
-export enum ButtonVariants{
-     normal,
+export enum ButtonVariants {
+    normal,
+    white
 }
 
-// demonstarce jak pridat novy typ buttonu 
+// demonstarce jak pridat novy typ buttonu
 
 let normalButtonStyle = b.styleDef({
     width: '50px',
     height: '50px',
     background: 'red'
+});
+
+let whiteButtonStyle = b.styleDef({
+    height: '45px',
+    width:  '150px',
+    borderRadius: '2px',
+    background: 'gray',
+    textAlign: 'center',
+});
+
+let whiteButtonStyleHover = b.styleDef({
+    background: 'white'
 });
 
 let normalButtonStyleHover = b.styleDef({
@@ -24,4 +36,11 @@ let normalButtonStyleHover = b.styleDef({
 
 export let ButtonsStyleMap: Map<ButtonVariants, IButtonStyles> = new Map();
 
-ButtonsStyleMap.set(ButtonVariants.normal, { style: normalButtonStyle,hoverStyle: normalButtonStyleHover });
+ButtonsStyleMap.set(ButtonVariants.normal, {
+    style: normalButtonStyle,
+    hoverStyle: normalButtonStyleHover
+});
+ButtonsStyleMap.set(ButtonVariants.white, {
+    style: whiteButtonStyle,
+    hoverStyle: whiteButtonStyleHover
+});
