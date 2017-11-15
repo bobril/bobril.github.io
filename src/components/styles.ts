ceee997799d60bd4245abf767a51c5882c1fb390
a.obrtel <a.obrtel@quadient.net>
2017-11-15 13:42:02 +0100

    pt to px change of units

--- src/components/styles.ts
+++ src/components/styles.ts
@@ -6,68 +6,68 @@ let size0 = 0;
 
 export const menutext01: b.IBobrilStyleDef = b.styleDef({
     fontFamily: 'Segoe UI',
-    letterSpacing: `${size75}pt`,
-    fontSize: '17pt',
+    letterSpacing: `${size75}px`,
+    fontSize: '17px',
     fontWeight: '600'
 });
 
 export const menutext01s: b.IBobrilStyleDef = b.styleDef({
     fontFamily: 'Segoe UI',
-    letterSpacing: `${size75}pt`,
-    fontSize: '16pt',
+    letterSpacing: `${size75}px`,
+    fontSize: '16px',
     fontWeight: '600'
 });
 
 export const menutext01b: b.IBobrilStyleDef = b.styleDef({
     fontFamily: 'Segoe UI',
-    letterSpacing: `${size0}pt`,
-    fontSize: '18pt',
+    letterSpacing: `${size0}px`,
+    fontSize: '18px',
     fontWeight: '600'
 });
 
 export const menutext02: b.IBobrilStyleDef = b.styleDef({
     fontFamily: 'Segoe UI',
-    letterSpacing: `${size75}pt`,
-    fontSize: '15pt',
+    letterSpacing: `${size75}px`,
+    fontSize: '15px',
     fontWeight: '700'
 });
 
 export const headertext01: b.IBobrilStyleDef = b.styleDef({
     fontFamily: 'Segoe UI',
-    letterSpacing: `${size75}pt`,
-    fontSize: '34pt',
+    letterSpacing: `${size75}px`,
+    fontSize: '34px',
     fontWeight: '600'
 });
 
 export const headertext02: b.IBobrilStyleDef = b.styleDef({
     fontFamily: 'Segoe UI',
-    letterSpacing: `${size75}pt`,
-    fontSize: '20pt',
+    letterSpacing: `${size75}px`,
+    fontSize: '20px',
     fontWeight: '400',
-    lineHeight: '24pt'
+    lineHeight: '24px'
 });
 
 export const buttontext01: b.IBobrilStyleDef = b.styleDef({
     fontFamily: 'Segoe UI',
-    letterSpacing: `${size75}pt`,
-    fontSize: '17pt',
+    letterSpacing: `${size75}px`,
+    fontSize: '17px',
     fontWeight: '700'
 });
 
 export const paragtext01: b.IBobrilStyleDef = b.styleDef({
     fontFamily: 'Segoe UI',
-    letterSpacing: `${size75}pt`,
-    fontSize: '17pt',
+    letterSpacing: `${size75}px`,
+    fontSize: '17px',
     fontWeight: '400',
-    lineHeight: '22pt'
+    lineHeight: '22px'
 });
 
 export const smalltext01: b.IBobrilStyleDef = b.styleDef({
     fontFamily: 'Segoe UI',
-    letterSpacing: `${size0}pt`,
-    fontSize: '14pt',
+    letterSpacing: `${size0}px`,
+    fontSize: '14px',
     fontWeight: '300',
-    lineHeight: '17pt'
+    lineHeight: '17px'
 });
 
 export const normalButton = b.styleDef({
