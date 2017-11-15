e2be6809986bc645d33efc3b11f5e682ac3b2a21
a.obrtel <a.obrtel@quadient.net>
2017-11-15 09:00:22 +0100

    appbar lib.ts was removed in merge of confilicts yesterday re-adding it :D

---
+++ src/components/appBar/lib.ts
@@ -0,0 +1,36 @@
+import * as b from 'bobril';
+import * as styles from '../styles';
+import * as colors from '../colors';
+
+import * as AppButton from './button';
+
+export const Button = AppButton;
+
+interface IData {
+    leftChildren: b.IBobrilChildren[];
+    rightChildren?: b.IBobrilChildren[];
+    contentWidth?: number;
+}
+
+interface IContext extends b.IBobrilCtx {
+    data: IData;
+}
+
+export const create = b.createComponent<IData>({
+    render(ctx: IContext, me: b.IBobrilNode) {
+        const d = ctx.data;
+
+        me.children = [
+            b.styledDiv(
+                [
+                    // TODO TOP BAR
+                ],
+                {
+                    minWidth: 1200,
+                    maxWidth: d.contentWidth,
+                    margin: 'auto'
+                }
+            )
+        ];
+    }
+});
