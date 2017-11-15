bd722f237009d6dbab133d9ea991c0a7923f4949
a.obrtel <a.obrtel@quadient.net>
2017-11-15 16:20:03 +0100

    rename menu_icon.png -> menuIcon.png

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -2,4 +2,4 @@ import * as b from 'bobril';
 
 export const bobrilLogo = b.asset('./img/bobrilLogoWhiteIntro.png');
 export const bobrilLogoInverse = b.asset('./img/bobrilInverse.png');
-export const menuIcon = b.asset('./img/menu_icon.png');
+export const menuIcon = b.asset('./img/menuIcon.png');
