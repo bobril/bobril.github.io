47b8a30208203f3a06615603044a4a7fb50e7710
a.obrtel <a.obrtel@quadient.net>
2017-11-15 08:29:09 +0100

    adding final button now we can have contetnt as any thing any button should resize to fit content

--- src/components/button/buttons.ts
+++ src/components/button/buttons.ts
@@ -1,24 +1,6 @@
 import * as b from 'bobril';
 
-export interface IButtonStyles {
-    style: b.IBobrilStyleDef;
-    hoverStyle?: b.IBobrilStyleDef;
-}
-
 export enum ButtonVariants {
     normal,
     white
 }
-
-// demonstarce jak pridat novy typ buttonu
-let whiteButtonStyle = b.styleDef({
-    height: '45px',
-    width:  '150px',
-    borderRadius: '2px',
-    background: 'gray',
-    textAlign: 'center',
-});
-
-let whiteButtonStyleHover = b.styleDef({
-    background: 'white'
-});
---
+++ src/components/styles.ts
@@ -0,0 +1,85 @@
+import * as b from 'bobril';
+
+let size75 = 0.75;
+let size0 = 0;
+
+export const menutext01enutext01: b.IBobrilStyleDef = b.styleDef({
+    fontFamily: 'Segoe UI',
+    letterSpacing: `${size75}pt`,
+    fontSize: '17pt',
+    fontWeight: '600'
+});
+
+export const menutext01s: b.IBobrilStyleDef = b.styleDef({
+    fontFamily: 'Segoe UI',
+    letterSpacing: `${size75}pt`,
+    fontSize: '17pt',
+    fontWeight: '600'
+});
+
+export const menutext01b: b.IBobrilStyleDef = b.styleDef({
+    fontFamily: 'Segoe UI',
+    letterSpacing: `${size0}pt`,
+    fontSize: '18pt',
+    fontWeight: '600'
+});
+
+export const menutext02: b.IBobrilStyleDef = b.styleDef({
+    fontFamily: 'Segoe UI',
+    letterSpacing: `${size75}pt`,
+    fontSize: '15pt',
+    fontWeight: '700'
+});
+
+export const headertext01: b.IBobrilStyleDef = b.styleDef({
+    fontFamily: 'Segoe UI',
+    letterSpacing: `${size75}pt`,
+    fontSize: '34pt',
+    fontWeight: '600'
+});
+
+export const headertext02: b.IBobrilStyleDef = b.styleDef({
+    fontFamily: 'Segoe UI',
+    letterSpacing: `${size75}pt`,
+    fontSize: '20pt',
+    fontWeight: '400',
+    lineHeight: '24pt'
+});
+
+export const buttontext01: b.IBobrilStyleDef = b.styleDef({
+    fontFamily: 'Segoe UI',
+    letterSpacing: `${size75}pt`,
+    fontSize: '34pt',
+    fontWeight: '700'
+});
+
+export const paragtext01: b.IBobrilStyleDef = b.styleDef({
+    fontFamily: 'Segoe UI',
+    letterSpacing: `${size75}pt`,
+    fontSize: '17pt',
+    fontWeight: '400',
+    lineHeight: '22pt'
+});
+
+export const smalltext01: b.IBobrilStyleDef = b.styleDef({
+    fontFamily: 'Segoe UI',
+    letterSpacing: `${size0}pt`,
+    fontSize: '14pt',
+    fontWeight: '300',
+    lineHeight: '17pt'
+});
+
+export const normalButton = b.styleDef({
+    width: '50px',
+    height: '50px',
+    background: 'red'
+});
+
+export const normalButtonHover = b.styleDef({
+    background: 'green'
+});
+
+export const buttonStyle = b.styleDef({
+    display: 'inline-block',
+    textAlign: 'center',
+})
